5127f1a60750051a8d69e690fb96c4411c624c5d
Bruno Basto <bruno.basto@liferay.com>
2014-12-29 15:39:36 -0300

    Update 2014-12-29T18:39:36.831Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 $(function() {
-    'use strict';
+	'use strict';
 
-    $('#search').fastLiveFilter('#list');
+	$.ajax({
+		dataType: 'json',
+		url: 'games.json'
+	})
+	.done(function(res) {
+		$("#gameTemplate").tmpl(res).appendTo("#gamesList");
+
+		$('#search').fastLiveFilter('#gamesList');
+	})
+	.fail(function(e) {
+		console.log('error', arguments);
+	})
+	.always(function() {
+		// alert( "complete" );
+	});
 });
